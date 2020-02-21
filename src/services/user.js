dea9ede0e85ae6f262ccf99abf31ee3209aa6920
LuisSky <luizguilhermeabrantes@gmail.com>
2020-02-20 22:40:56 -0300

    Blocked submission the null names

--- src/services/user.js
+++ src/services/user.js
@@ -1,12 +1,11 @@
 module.exports = (app) => {
-  
   const findAll = () => {
     return app.db('users').select()
   };
-
   const save = (user) => {
+    if (!user.name) return { error: 'Name is mandatory submission' };
+
     return app.db('users').insert(user, '*');
   };
-  
   return { findAll, save };
 };
--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -19,3 +19,12 @@ test('insert user for post method', () => {
       expect(res.body[0]).toHaveProperty('name', 'Sky Nesk');
     });
 });
+
+test('Blocking null name submission', () => {
+  return request(app).post('/users')
+    .send({ mail: 'jhon@mail.com', password: '123456' })
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Name is mandatory submission');
+    });
+});
