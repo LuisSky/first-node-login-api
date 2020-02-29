bc56e1944c46f8c3457ecc4af1662aaefb8c58b4
LuisSky <luizguilhermeabrantes@gmail.com>
2020-02-29 16:54:10 -0300

    inserting generic treatment for errors

--- src/services/user.js
+++ src/services/user.js
@@ -1,14 +1,16 @@
+const ValidationError = require('../errors/ValidationError.js');
+
 module.exports = (app) => {
   const findAll = (filter = {}) => {
     return app.db('users').where(filter).select();
   };
   const save = async (user) => {
-    if (!user.name) return { error: 'Name is mandatory submission' };
-    if (!user.mail) return { error: 'Mail is mandatory submission' };
-    if (!user.password) return { error: 'Password is mandatory submission' };
+    if (!user.name) throw new ValidationError('Name is mandatory submission');
+    if (!user.mail) throw new ValidationError('Mail is mandatory submission');
+    if (!user.password) throw new ValidationError('Password is mandatory submission');
 
     const userDb = await findAll({ mail: user.mail });
-    if (userDb && userDb.length > 0) return { error: 'Mail is already registered' };
+    if (userDb && userDb.length > 0) throw new ValidationError('Mail is already registered');
 
     return app.db('users').insert(user, '*');
   };
